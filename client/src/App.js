136f2c8c41c6e89c4275510413009c4b95c6695d
Soo <sooyc26@gmail.com>
2018-11-09 14:47:24 -0800

    renew

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import LyricsForm from './forms/lyricsForm';
-import Header from './layout/Header';
-import SideBar from './layout/SideBar';
 
 class App extends Component {
-
   render() {
-
     return (
-      <div >
-        {/* <Header /> */}
-        {/* <SideBar /> */}
-        <LyricsForm />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
